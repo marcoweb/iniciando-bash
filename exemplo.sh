b1031c576fa170fa4bf165de70621a363b87c47a
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2024-05-18 01:43:28 +0000

    Arquivo de Exemplo

---
+++ exemplo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#echo "Tempo de uso do PC"
+# Chamada de Comandos
+# uptime
+#var=Teste
+#echo "$var"
+echo "Digite um número inteiro: "
+read numero
+
+for num in {1..10}
+do
+    echo $numero " * " $num " = " $(($numero * $num))
+done
+
+if [$(($numero % 2)) -eq 0]; then
+    echo "Par"
+fi
